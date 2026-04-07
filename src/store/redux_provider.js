3d2b58c5a37d8c57b16f4f668545f689cac0ef87
AsifRiseofPower <aloasif31@gmail.com>
2026-04-08 04:15:22 +0530

    fix it thing

--- src/store/redux_provider.js
+++
@@ -1,8 +0,0 @@
-import { Provider } from "react-redux";
-import { makeStore } from "./store";
-
-const store = makeStore();
-
-export function ReduxProvider({ children }) {
-  return <Provider store={store}>{children}</Provider>;
-}
